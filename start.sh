851615024132cb5e40d5fbbf91ae1047749e8822
richasdy <donni.richasdy@gmail.com>
2018-02-13 06:22:10 +0700

    add apex

--- start.sh
+++ start.sh
@@ -1,1 +1,21 @@
 docker build -t richasdy/docker-oracle-xe-11g .
+# docker run -d -p 49160:22 -p 49161:1521 richasdy/docker-oracle-xe-11g
+# docker run -d -p 49160:22 -p 49161:1521 -e ORACLE_ALLOW_REMOTE=true richasdy/docker-oracle-xe-11g
+# docker run -d -p 49160:22 -p 49161:1521 -e ORACLE_PASSWORD_VERIFY=true richasdy/docker-oracle-xe-11g
+# docker run -d -p 49160:22 -p 49161:1521 -e ORACLE_DISABLE_ASYNCH_IO=true richasdy/docker-oracle-xe-11g
+docker run -d -p 49160:22 -p 49161:1521 -p 8080:8080 -e ORACLE_ENABLE_XDB=true richasdy/docker-oracle-xe-11g
+
+# docker stop richasdy/docker-oracle-xe-11g
+
+
+# https://github.com/richasdy/docker-oracle-xe-11g
+
+# CREDENTIAL
+# hostname: localhost
+# port: 49161
+# sid: xe
+# username: system
+# password: oracle
+
+#SSH
+# ssh root:admin@localhost -p 49160
